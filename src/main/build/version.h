4d265dc4e6e2e8456e8e85be00fcbd1d60c433b2
Till Blaha <28341894+tblaha@users.noreply.github.com>
2023-09-26 13:43:51 +0200

    bump version and firmware name

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -22,11 +22,11 @@
 
 #include "common/utils.h"
 
-#define FC_FIRMWARE_NAME            "Betaflight"
-#define FC_FIRMWARE_IDENTIFIER      "BTFL"
-#define FC_VERSION_MAJOR            4  // increment when a major release is made (big new feature, etc)
-#define FC_VERSION_MINOR            4  // increment when a minor release is made (small new feature, change etc)
-#define FC_VERSION_PATCH_LEVEL      2  // increment when a bug is fixed
+#define FC_FIRMWARE_NAME            "Indiflight"
+#define FC_FIRMWARE_IDENTIFIER      "INFL"
+#define FC_VERSION_MAJOR            5  // increment when a major release is made (big new feature, etc)
+#define FC_VERSION_MINOR            0  // increment when a minor release is made (small new feature, change etc)
+#define FC_VERSION_PATCH_LEVEL      0  // increment when a bug is fixed
 
 #define FC_VERSION_STRING STR(FC_VERSION_MAJOR) "." STR(FC_VERSION_MINOR) "." STR(FC_VERSION_PATCH_LEVEL)
 
